11fed8d80f1c7fcbc8be5a578d9e1448a75ba5a6
ppak10 <37542469+ppak10@users.noreply.github.com>
2018-10-01 16:44:56 -0400

    Hello World
    Pure implementation of Express and Babel modules without using express-generator

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ const app = express()
 const port = 3000
 
 app.get('/', (request, response) => response.send('Hello World'))
-app.listen(port, () => console.log('Express app listening on port: '+port))
+app.listen(port, () => console.log('Express app listening on port: ' + port))
